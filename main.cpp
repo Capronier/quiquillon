ff42ee33104b5fe2b51d826e319a75b61f6bd977
Capronier <z.yann@hotmail.fr>
2020-01-24 20:14:58 +0100

    test2

--- main.cpp
+++ main.cpp
@@ -4,22 +4,23 @@ using namespace std;
 
 int main()
 {
- int x(101);
- int y(101);
- int map[x][y];
-  for (int i = 0; i < x; i++){
-    for (int j = 0; j < y; j++){
-      map[i][j]=0;
+  int x(101);
+  int y(101);
+  int map[x][y];
+  for (int i = 0; i < x; i++)
+  {
+    for (int j = 0; j < y; j++)
+    {
+      map[i][j] = i+j;
     }
   }
- std::ofstream ofs("map.txt");
- return 0;
- for (int i = 0; i < x; i++){
-  for (int j = 0; j < y; j++){
-   ofs << map[i][j] << std::endl;
+  std::ofstream ofs("map.txt");
+  for (int i = 0; i < x; i++) {
+    for (int j = 0; j < y; j++) {
+      ofs << map[i][j] << std::endl;
+    }
   }
- }
- ofs << "test";
- return 0;
+  ofs << "test";
+  return 0;
 }
 
